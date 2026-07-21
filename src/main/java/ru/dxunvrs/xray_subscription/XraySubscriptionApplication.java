b29ac0c9de64a0c1592f47ea72abad7ad865fe0e
roma-dxunvrs <r.shulai@yandex.ru>
2026-07-21 14:39:34 +0300

    Init project

---
+++ src/main/java/ru/dxunvrs/xray_subscription/XraySubscriptionApplication.java
@@ -0,0 +1,13 @@
+package ru.dxunvrs.xray_subscription;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XraySubscriptionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XraySubscriptionApplication.class, args);
+	}
+
+}
